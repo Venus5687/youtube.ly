e5e7d5d3046431f54d1289da7af8eca88498068d
Venus5687 <96628563+Venus5687@users.noreply.github.com>
2026-04-08 17:15:25 +0200

    fix users a bit and show a placeholder notif

---
+++ listnotif.php
@@ -0,0 +1,72 @@
+<?php
+header("Content-Type: application/json");
+echo '{
+    "success": true,
+    "result": {
+        "content": [
+            {
+                "checked": null,
+				"deviceMessage": "Hello and welcome to youtube.ly! The notification system has not been finished yet.",
+				"insertTime": 1774386849968,
+				"message": "Hello and welcome to youtube.ly! The notification system has not been finished yet.",
+				"notifyBy": {},
+				"notifyId": 1,
+				"notifytTime": 1774386849968,
+				"notifyTo": {
+		"userId": 1,
+		"userIdStr": "1",
+		"bid": "1",
+		"nickName": "820182",
+		"handle": "820182",
+		"gender": "m",
+		"icon": null,
+		"userDesc": "live with passion, live musical.ly",
+		"verified": false,
+		"featured": false,
+		"featuredScope": 0,
+		"followed": false,
+		"following": false,
+		"blocked": false,
+		"complimented": false,
+		"postNotify": false,
+		"instagramID": null,
+		"introduction": null,
+		"handleModified": null,
+		"fansNum": 0,
+		"followNum": 0,
+		"likesNum": 0,
+		"musicalNum": 0,
+		"musicalLikedNum": 0,
+		"musicalReadNum": 0,
+		"email": "example@example.com",
+		"sociaMediaList": [],
+		"userRequestDTO": {
+			"follow": false
+		},
+		"userSettingDTO": {
+			"userId": 1,
+			"secret": false,
+			"duet": false
+		}
+	},
+	"notifyType": 0,
+	"refBid": "1",
+	"refId": 1,
+	"refImgPath": null,
+	"subNotifyType": 0,
+	"subject": "Welcome!",
+	"url": null
+            }
+        ],
+        "total": 0,
+        "number": 0,
+        "size": 20,
+        "totalPages": 0,
+        "totalElements": 0,
+        "numberOfElements": 0,
+        "firstPage": true,
+        "lastPage": true
+    },
+    "timestamp": 1774386849968,
+    "fail": false
+}';
