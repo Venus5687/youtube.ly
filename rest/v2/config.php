0683c4593a7124d27e96b0437d4a33122e034580
Venus5687 <96628563+Venus5687@users.noreply.github.com>
2026-04-17 11:27:34 +0200

    Update topSong output

--- rest/v2/config.php
+++ rest/v2/config.php
@@ -9,7 +9,7 @@ echo json_encode([
 	"errorTitle" => null,
 	"fail" => false,
     "result" => [
-		"content" => '[{"trackId": 1, "song": "New Friendly","author": "Kevin MacLeod","champion": "New Friendly","cover" : null},{"trackId": 2, "song": "Itty Bitty 8 Bit","author": "Kevin MacLeod","champion": "Itty Bitty 8 Bit","cover" : null},{"trackId": 3, "song": "Fig Leaf Times Two","author": "Kevin MacLeod","champion": "Fig Leaf Times Two","cover" : null},]',
+		"content" => '[{"trackId": 1, "song": "New Friendly","author": "Kevin MacLeod","champion": "New Friendly","cover" : null},{"trackId": 2, "song": "Itty Bitty 8 Bit","author": "Kevin MacLeod","champion": "Itty Bitty 8 Bit","cover" : null},{"trackId": 3, "song": "Fig Leaf Times Two","author": "Kevin MacLeod","champion": "Fig Leaf Times Two","cover" : null}]',
 				 "itemId" => 3,
 				 "itemKey" => "topSong",
 				 "regionCode" => 2
