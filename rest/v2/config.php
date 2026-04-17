338f2a092062b4de7a634f6f17139d43b2646c7f
Venus5687 <96628563+Venus5687@users.noreply.github.com>
2026-04-17 11:09:42 +0200

    Update config.php
    update topSong output

--- rest/v2/config.php
+++ rest/v2/config.php
@@ -2,27 +2,19 @@
 if ($_GET["key"] == "topSong") {
 header('Content-Type: application/json');
 echo json_encode([
-    [
-            "trackId"  => 1,
-            "song"     => "New Friendly",
-            "author"   => "Kevin MacLeod",
-            "champion" => "Kevin MacLeod",
-            "cover"    => null
-        ],
-        [
-            "trackId"  => 2,
-            "song"     => "Itty Bitty 8 Bit",
-            "author"   => "Kevin MacLeod",
-            "champion" => "Kevin MacLeod",
-            "cover"    => null
-        ],
-        [
-            "trackId"  => 3,
-            "song"     => "Fig Leaf Times Two",
-            "author"   => "Kevin MacLeod",
-            "champion" => "Kevin MacLeod",
-            "cover"    => null
-		]
+    "success" => true,
+	"advItems" => [],
+	"errorCode" => null,
+	"errorMsg" => null,
+	"errorTitle" => null,
+	"fail" => false,
+    "result" => [
+		"content" => '[{"trackId": 1, "song": "New Friendly","author": "Kevin MacLeod","champion": "New Friendly","cover" : null},{"trackId": 2, "song": "Itty Bitty 8 Bit","author": "Kevin MacLeod","champion": "Itty Bitty 8 Bit","cover" : null},{"trackId": 3, "song": "Fig Leaf Times Two","author": "Kevin MacLeod","champion": "Fig Leaf Times Two","cover" : null},]',
+				 "itemId" => 3,
+				 "itemKey" => "topSong",
+				 "regionCode" => 2
+    ],
+    "timestamp" => round(microtime(true) * 1000)
     
 ]);
 } else if ($_GET["key"] == "parameters") {
